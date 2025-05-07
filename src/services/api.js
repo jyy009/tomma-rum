ee122fbec1fdb096f3eac6cc8b1e9681f4956ec0
Naghmeh Ghorashi <naghmeh.ghorashi@gmail.com>
2025-05-07 14:13:47 +0200

    add fetching function in services file

---
+++ src/services/api.js
@@ -0,0 +1,16 @@
+const BASE_URL = "https://tommarum.se/wp-json/wp/v2"
+
+export async function getArtProjects() {
+  try {
+    const res = await fetch(`${BASE_URL}/posts`)
+
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`)
+    }
+
+    return await res.json()
+  } catch (error) {
+    console.error("Fetch Error:", error.message || error)
+    throw error
+  }
+}
