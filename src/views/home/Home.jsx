092630c64561a7cfc7df96e12e3952ca506ec620
Mohamed Guled <mohamed.guled@hyperisland.se>
2025-05-07 19:24:20 +0200

    Add Home.css and update Home.jsx with new styling

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,7 +1,19 @@
-import React from "react"
+import React from "react";
+import "./Home.css"
 
 function Home() {
-  return <div className="text-6xl">home page</div>
+  return (
+    <div className="home-container">
+      <h1 className="home-title">
+        Tomma Rum är en självorganiserad plattform för kulturellt och konstnärligt utbyte
+      </h1>
+      <p className="home-description">
+        Varje sommar arrangerar alla deltagare tillsammans en vistelse på en mindre ort i Sverige. 
+        Tomma rum handlar om att mötas och att skapa nya former av samarbeten mellan konstnärer, 
+        kulturutövare och lokalbefolkningen på den ort vi besöker.
+      </p>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
