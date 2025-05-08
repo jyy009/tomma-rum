28cc147ee0145584938c92a4eb419e979d286685
Ji <153378401+jyy009@users.noreply.github.com>
2025-05-08 21:45:09 +0200

    Render image for project card

--- src/components/sharedComponents/project/project.jsx
+++ src/components/sharedComponents/project/project.jsx
@@ -1,12 +1,20 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 
-function Project({ title, date }) {
+function Project({ id, title, date, content }) {
   const formatDate = (date) => {
     return date.slice(0, 10)
   }
 
+  const extractFirstImage = (html) => {
+    const match = html.match(/<img[^>]+src="([^">]+)"/)
+    return match ? match[1] : null
+  }
+
+  const imageUrl = extractFirstImage(content.rendered)
+
   return (
     <section>
+      <img src={imageUrl} alt={title.rendered} />
       <h2>{title.rendered}</h2>
       <p>{formatDate(date)}</p>
     </section>
