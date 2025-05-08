857ee254ca66a94032aae8ed438ff6509d71add3
Sigstus985 <siggeluren@gmail.com>
2025-05-08 11:32:40 +0200

    added all content to the footer

--- src/components/sharedComponents/footer/Footer.jsx
+++ src/components/sharedComponents/footer/Footer.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React from "react"
+import { MdOutlineHomeWork } from "react-icons/md"
 
 function Footer() {
-  return <div>footer</div>;
+  return (
+    <footer className="bg-stone-900 absolute bottom-0 w-screen">
+      <div className="flex content-center">
+        <MdOutlineHomeWork className="" />
+        <h3 className="text-3xl">Tomma Rum</h3>
+      </div>
+      <ul>
+        <li>Om oss</li>
+        <li>Instagram</li>
+        <li>Facebook</li>
+        <li>Mail</li>
+        <li>2025</li>
+      </ul>
+    </footer>
+  )
 }
 
-export default Footer;
+export default Footer
