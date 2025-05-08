12408ebcdcece3529239ab95fc76d30bfbb8f707
Sigstus985 <siggeluren@gmail.com>
2025-05-08 15:17:48 +0200

    added tomma rum logo instead of placeholder

--- src/components/sharedComponents/footer/Footer.jsx
+++ src/components/sharedComponents/footer/Footer.jsx
@@ -3,23 +3,22 @@ import { MdOutlineHomeWork } from "react-icons/md"
 
 function Footer() {
   return (
-    <footer className="bg-stone-950 absolute bottom-0 w-screen text-center font-bold p-3 sm:flex sm:justify-between px-10">
-      <div className="flex justify-center items-center">
-        <MdOutlineHomeWork className="h-12 w-12" />
-        <h3 className="text-3xl h-min">Tomma Rum</h3>
+    <footer className="bg-stone-950 text-stone-50 absolute bottom-0 w-screen text-center font-bold p-3 sm:flex sm:justify-between px-10 md:pr-20">
+      <div className="flex justify-center items-center ">
+        <img src="src/assets/tommarum-logo_TR-WHITE.png" alt="the tomma rum logo" className=" w-100 mb-5 sm:mb-0"/>
       </div>
       <ul>
-        <li className="text-xl ">
+        <li className="text-xl">
           {" "}
           <a href="/about">Om oss</a>
         </li>
         <li className="text-xl">
           {" "}
-          <a href="https://www.instagram.com/tomma_rum?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==">Instagram</a>
+          <a href="https://www.instagram.com/tomma_rum?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank">Instagram</a>
         </li>
         <li className="text-xl">
           {" "}
-          <a href="https://www.facebook.com/tommarum.sweden">Facebook</a>
+          <a href="https://www.facebook.com/tommarum.sweden" target="_blank">Facebook</a>
         </li>
         <li className="text-xl">
           {" "}
