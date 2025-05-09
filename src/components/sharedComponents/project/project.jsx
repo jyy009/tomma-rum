ff2292e4357ccebb0363b8593be75886f07bee08
Ji <153378401+jyy009@users.noreply.github.com>
2025-05-09 21:57:03 +0200

    Add pagination

--- src/components/sharedComponents/project/project.jsx
+++ src/components/sharedComponents/project/project.jsx
@@ -10,15 +10,27 @@ function Project({ title, date, content, id }) {
     const match = html.match(/<img[^>]+src="([^">]+)"/)
     return match ? match[1] : null
   }
+  function extractAllImages(html) {
+    const matches = [...html.matchAll(/<img[^>]+src="([^">]+)"/g)]
+    return matches.map((match) => match[1])
+  }
 
-  const imageUrl = extractFirstImage(content.rendered)
+  const imageUrl = extractAllImages(content.rendered)
 
   return (
     <section>
-      <img src={imageUrl} alt={title.rendered} className="w-[407px] h-[282px] object-cover"/>
+      <img
+        src={imageUrl}
+        alt={title.rendered}
+        className="w-[407px] h-[282px] object-cover"
+      />
+      {/* <div
+        className="w-[407px] h-[282px] object-cover"
+        dangerouslySetInnerHTML={{ __html: content.rendered }}
+      /> */}
       <h2>{title.rendered}</h2>
       <p>{formatDate(date)}</p>
-      <Button projectId={id}/>
+      <Button projectId={id} />
     </section>
   )
 }
