42e928507a7dada13b83c396b126feb06a1576b1
Mohamed Guled <mohamed.guled@hyperisland.se>
2025-05-13 00:40:29 +0200

    added information from api to cards, should display the 3 latest post on the home page

--- src/views/home/components/Nyheter.jsx
+++ src/views/home/components/Nyheter.jsx
@@ -16,7 +16,7 @@ function Nyheter() {
 
   if (isLoading) return (
     <section className="container mx-auto px-4 py-12">
-      <h2 className="text-4xl font-bold mb-12 text-center text-black">Nyheter</h2>
+      <h2 className="text-4xl font-bold mb-12 text-black">Nyheter</h2>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
         {[...Array(3)].map((_, i) => (
           <div key={i} className="animate-pulse flex flex-col items-center">
